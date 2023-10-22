9f318fdcd826e28f53decc2e21b3f0866eae5c39
L364CY <78699541+L364CY-FM@users.noreply.github.com>
2023-10-22 08:19:47 +0200

    Update user.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,68 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    -- config = function()
+    --   require("todo-comments").setup{}
+    -- end,
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+    },
+    config = true,
+    event = "User AstroFile",
+  },
+  {
+    "lervag/vimtex",
+    ft = { "latex" },
+    config = function()
+      Syntax = true
+      vim.g.vimtex_view_method = "skim"
+      vim.g.vimtex_compiler_engine = "lualatex"
+    end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    ft = "markdown",
+    lazy = true,
+    keys = { { "<leader>lp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Preview" } },
+    config = function()
+      vim.g.mkdp_auto_close = true
+      vim.g.mkdp_open_to_the_world = false
+      vim.g.mkdp_open_ip = "127.0.0.1"
+      vim.g.mkdp_port = "8888"
+      vim.g.mkdp_browser = ""
+      vim.g.mkdp_echo_preview_url = true
+      vim.g.mkdp_page_title = "${name}"
+    end,
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
+    "f-person/git-blame.nvim",
+    event = "User AstroFile",
+    config = true,
+  },
 }
